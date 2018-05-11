2551f2f1185c5e235aca437f1b63e5c6d80c3a84
chenhui <ch15110082540@163.com>
2018-05-11 19:35:20 +0800

    修改快排

--- javascript/quicksort/src/quicksort.js
+++ javascript/quicksort/src/quicksort.js
@@ -1,18 +1,16 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
-    let pivotIndex = Math.floor(arr.length / 2);
-    let pivot = arr.splice(pivotIndex, 1)[0];
-    let left = [];
-    let right = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-    // 递归
-    return quickSort(left).concat([pivot], quickSort(right));
+function qSort(arr) {
+  if (arr.length==0) {
+      return [];
+  }
+  var left = [];
+  var right = [];
+  var pivot = arr[0];
+  for (var i =1; i <arr.length; i++) { // 注意这里的起始值，因为有一个作为flag了
+     if (arr[i] < pivot) {
+           left.push(arr[i]);
+      } else {
+          right.push(arr[i]);
+      }
+  }
+  return qSort(left).concat(pivot, qSort(right));
 }
